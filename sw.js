9066d1193a204dfe194f505f4e03333b0b2986fd
dazzdg-dev <dazz.dg@gmail.com>
2025-11-14 23:05:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { clients.claim(); });
-self.addEventListener('fetch', e => {});
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+
+// Simple pass-through fetch (no offline cache yet)
+self.addEventListener('fetch', event => {
+  // You can add caching here later if you want
+});
